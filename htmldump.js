8e43518c9f338bc6fd565f10fbcf6e2419927f5a
antimatter15 <antimatter15@gmail.com>
2010-09-24 00:42:11 +0000

    this seems to work best

--- htmldump.js
+++ htmldump.js
@@ -1,46 +1,67 @@
+/*
 function getDefaults(){
   var frame = document.createElement('iframe') //create an iframe to get defaults unaffected by css
   document.body.appendChild(frame);
   var win = frame.contentWindow;
-  var el = win.document.createElement('klsdfjalksdfj');
+  var el = win.document.createElement('div');
+  var el2 = win.document.createElement('div');
   win.document.body.appendChild(el);
+  el.appendChild(el2);
   var style = win.getComputedStyle(el, null);
+  var style2 = win.getComputedStyle(el, null);
   
   var s = {};
-  for(var i = style.length; i--;)
-    s[style[i]] = style[style[i]];
-  
+  for(var i = style.length; i--;){
+    if(style[style[i]] == style2[style[i]]){
+      s[style[i]] = style[style[i]];
+    }
+  }
   document.body.removeChild(frame);
 
   return s;
 }
+var defaults = {}//getDefaults();
 
-var defaults = {};
+*/
 
 function getHTML(win, el){
+  var tagName = el.tagName.toLowerCase();
+  if(tagName == 'br') return '<br>';
   var style = win.getComputedStyle(el, null);
+  var parentStyle = win.getComputedStyle(el.parentNode, null);
   if(style.display == 'none') return ''; //dont show hidden elements
   var cssText = '';
 
+  //var inheritable = 'azimuth,border-collapse,border-spacing,caption-side,color,cursor,direction,elevation,empty-cells,font-family,font-size,font-style,font-variant,font,letter-spacing,line-height,list-style-image,list-style-position,list-style-type,list-style,orphans,pitch-range,pitch,quotes,richness,speak-header,speak-numeral,speak-punctuation,speak,speech-rate,stress,text-align,text-indent,text-transform,visibility,voice-family,volume,white-space,widows,word-spacing'.split(',');
+  
 
-
-  for(var i = style.length; i--;)
-    if(style[i].charAt(0) != '-'){
-      if(defaults[style[i]] != style[style[i]]) 
-        cssText += style[i] + ':' + style[style[i]] + ';';
-    }
-
-  var attrtext = ['style="'+cssText+'"'];
-  for(var i = el.attributes.length; i--;){
-    var name = el.attributes[i].name.toLowerCase();
-    if(name != 'style' && name != 'className' && name != 'id' && name != 'name' && name != 'class'){
-      attrtext.push([el.attributes[i].name+'="'+el.attributes[i].value+'"']);
+  for(var i = style.length; i--;){
+    var prop = style[i];
+    if(prop.charAt(0) != '-'){
+    
+    //if(defaults[prop] != style[prop] && !(parentStyle[prop] == style[prop] && inheritable.indexOf(prop) != -1)) 
+        cssText += prop + ':' + style[prop] + ';';
     }
   }
 
   
-  var header = '<'+el.tagName.toLowerCase() + ' ' + attrtext.join(' ')+'>';
-  var footer = '<'+'/'+el.tagName.toLowerCase() + '>';
+  var attrtext = ['style="'+cssText+'"'];
+
+  if(el.value){
+    attrtext.push('value="'+el.value+'"');
+  }
+  
+  for(var i = el.attributes.length; i--;){
+    var name = el.attributes[i].name.toLowerCase();
+    if(name != 'style' && name != 'className' && name != 'id' && name != 'name' && name != 'class' && name != 'value'){
+      attrtext.push(el.attributes[i].name+'="'+el.attributes[i].value+'"');
+    }
+  }
+  
+
+  
+  var header = '<'+tagName + ' ' + attrtext.join(' ')+'>';
+  var footer = '<'+'/'+tagName + '>';
   var html = '';
   for(var i = 0, l = el.childNodes.length; i < l; i++){
     var node = el.childNodes[i];
@@ -52,3 +73,5 @@ function getHTML(win, el){
   }
   return header + html + footer;
 }
+
+document.documentElement.innerHTML = getHTML(window, document.body);
